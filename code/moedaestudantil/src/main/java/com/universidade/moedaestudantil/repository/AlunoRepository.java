497aa61c06b3681c5c1e652065c4e7fce181702e
JoaoFerreira479 <164263649+JoaoFerreira479@users.noreply.github.com>
2025-11-11 02:10:08 -0300

    Delete code directory

--- code/moedaestudantil/src/main/java/com/universidade/moedaestudantil/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.universidade.moedaestudantil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.universidade.moedaestudantil.model.Aluno;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
