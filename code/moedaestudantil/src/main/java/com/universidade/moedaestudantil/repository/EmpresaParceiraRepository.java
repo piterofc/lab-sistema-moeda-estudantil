635c28bdd153e53e83e8077b68a3331904e1cf3c
JoaoFerreira479 <164263649+JoaoFerreira479@users.noreply.github.com>
2025-11-17 16:47:40 -0300

    Delete code directory

--- code/moedaestudantil/src/main/java/com/universidade/moedaestudantil/repository/EmpresaParceiraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.universidade.moedaestudantil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.universidade.moedaestudantil.model.EmpresaParceira;
-
-public interface EmpresaParceiraRepository extends JpaRepository<EmpresaParceira, Long> {
-}
--- code/moedaestudantil/src/main/java/com/universidade/moedaestudantil/repository/InstituicaoEnsinoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.universidade.moedaestudantil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.universidade.moedaestudantil.model.InstituicaoEnsino;
-
-public interface InstituicaoEnsinoRepository extends JpaRepository<InstituicaoEnsino, Long> {
-}
