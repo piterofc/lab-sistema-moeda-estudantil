7d37c397b21945158b896341cb8ac639ecdfba87
JoaoFerreira479 <164263649+JoaoFerreira479@users.noreply.github.com>
2025-11-18 00:27:18 -0300

    Delete implementacao directory

--- implementacao/back/moedaestudantil/src/main/java/com/universidade/moedaestudantil/repository/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.universidade.moedaestudantil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.universidade.moedaestudantil.model.Professor;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
